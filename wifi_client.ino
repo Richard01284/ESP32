2a4cbfb2eb803a8f78f705fdec45460bf606243a
Richard <richardgallant108@gmail.com>
2026-04-07 08:49:31 +0200

    Update wifi_client.ino

--- wifi_client.ino
+++ wifi_client.ino
@@ -27,3 +27,43 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+#include <WiFi.h>
+
+const char* ssid = "ESP32_Server";
+const char* password = "12345678";
+
+const char* host = "192.168.4.1"; // Server IP
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  WiFiClient client;
+
+  if (client.connect(host, 80)) {
+    Serial.println("Connected to Server");
+
+    client.println("Hello from ESP32 Client");
+
+    while (client.available()) {
+      String response = client.readStringUntil('\n');
+      Serial.println("Server: " + response);
+    }
+
+    client.stop();
+  } else {
+    Serial.println("Connection failed");
+  }
+
+  delay(3000);
+}
